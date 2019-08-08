6ad7ee63661c818273292266a964e7264a5cbf81
mghasemi <mehdi.ghasemi@gmail.com>
2019-08-08 08:33:33 -0600

    Example and data

--- examples/example01.py
+++ examples/example01.py
@@ -21,10 +21,10 @@ s_date = datetime(year=2018, month=1, day=1)
 instance = InventoryOptim(df, date_fld='ds', units_costs=pairs, start_date=s_date,
 			num_intrvl=(0., 1.), projection_date=datetime(year=2021, month=1, day=1),
 			c_limit=.95)
-instance.set_unit_count_regressor(sgd_regressor)
+instance.set_unit_count_regressor(LinearRegression())
 instance.set_cost_regressor(LinearRegression())
 instance.fit_regressors()
-instance.plot_init_system().savefig('init.png', dpi=200).show()
+instance.plot_init_system().savefig('init.png', dpi=200)
 instance.budget = lambda t: 30000-22000*np.exp(-t-1.)
 # constraints
 instance.constraint('GH', 650, datetime(year=2021, month=1, day=1))
@@ -36,4 +36,4 @@ instance.constraint('OPc', 3.5, datetime(year=2020, month=1, day=1))
 # run
 instance.adjust_system(tbo='b')
 fig = instance.plot_analysis()
-fig[1].show()
+fig[1].savefig('results.png', dpi=200)
